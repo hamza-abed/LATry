14ea028b094e9cbbde186703266bbbb4b22aded5
hamzaf16 <hamza.abed.professionel@gmail.com>
2014-03-18 14:11:42 +0100

    Ajout d'une flèche pour sèlectionner la destination du joueur.

--- src/dynamicLoad/Main.java
+++ src/dynamicLoad/Main.java
@@ -10,19 +10,23 @@ import com.jme3.bullet.collision.shapes.CollisionShape;
 import com.jme3.bullet.control.CharacterControl;
 import com.jme3.bullet.control.RigidBodyControl;
 import com.jme3.bullet.util.CollisionShapeFactory;
+import com.jme3.collision.CollisionResults;
 import com.jme3.font.BitmapText;
 import com.jme3.input.ChaseCamera;
 import com.jme3.input.KeyInput;
 import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.AnalogListener;
 import com.jme3.input.controls.KeyTrigger;
+import com.jme3.input.controls.MouseButtonTrigger;
 import com.jme3.light.AmbientLight;
 import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.FastMath;
+import com.jme3.math.Matrix4f;
 import com.jme3.math.Plane;
 import com.jme3.math.Quaternion;
+import com.jme3.math.Ray;
 import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
 import com.jme3.niftygui.NiftyJmeDisplay;
@@ -30,22 +34,22 @@ import com.jme3.post.FilterPostProcessor;
 import com.jme3.renderer.RenderManager;
 import com.jme3.renderer.queue.RenderQueue;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Mesh;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
+import com.jme3.scene.debug.Arrow;
 import com.jme3.scene.shape.Quad;
 import com.jme3.system.AppSettings;
+import com.jme3.terrain.Terrain;
 import com.jme3.terrain.geomipmap.TerrainQuad;
 import com.jme3.terrain.heightmap.AbstractHeightMap;
 import com.jme3.terrain.heightmap.ImageBasedHeightMap;
-import com.jme3.terrain.heightmap.RawHeightMap;
 import com.jme3.texture.Texture;
 import com.jme3.texture.Texture.WrapMode;
 import com.jme3.texture.Texture2D;
 import com.jme3.water.SimpleWaterProcessor;
 import com.jme3.water.WaterFilter;
 import de.lessvoid.nifty.Nifty;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * test
@@ -84,6 +88,87 @@ public class Main extends SimpleApplication implements AnalogListener,AnimEventL
     private Quad quad;
     private WaterFilter water;
 
+    public Main() {
+        this.actionListener = new ActionListener() { 
+public void onAction(String name, boolean keyPressed, float tpf) {
+if (name.equals("addObject")) {
+//Node fireCamp=(Node) assetManager.loadModel("Models/Oto/Oto.mesh.xml");
+Spatial fireCamp = assetManager.loadModel("Models/campfire/campfire.j3o");
+rootNode.attachChild(fireCamp);
+fireCamp.setLocalTranslation(new Vector3f(player.getPhysicsLocation().x+30,player.getPhysicsLocation().y,player.getPhysicsLocation().z));
+System.out.println("child attached !!");
+
+}
+if(name.equals("LClick"))
+{
+afficherFlecheDestination();
+}
+
+}
+};  }
+
+ private Arrow arrow;
+
+ public void afficherFlecheDestination()
+    {
+        
+///This is about arrow
+arrow = new Arrow(Vector3f.UNIT_Y);
+arrow.setLineWidth(4); // make arrow thicker
+
+Vector3f origin    = sinbadPlayer.getLocalTranslation();
+Vector3f direction = cam.getWorldCoordinates(inputManager.getCursorPosition(), 0.3f);
+
+
+Vector3f location = direction.subtractLocal(origin).normalizeLocal();
+//putShape(arrow, ColorRGBA.Green).setLocalTranslation(
+//new Vector3f(location.x, location.y,location.z)
+//); 
+//System.out.println("location "+location + "sinbad "+sinbadPlayer.getLocalTranslation());
+
+
+/////
+ // 1. Reset results list.
+CollisionResults results = new CollisionResults();
+Vector2f click2d = inputManager.getCursorPosition();
+Vector3f click3d = cam.getWorldCoordinates(
+    new Vector2f(click2d.x, click2d.y), 0f).clone();
+Vector3f dir = cam.getWorldCoordinates(
+    new Vector2f(click2d.x, click2d.y), 1f).subtractLocal(click3d).normalizeLocal();
+Ray ray = new Ray(click3d, dir);
+sceneModel.collideWith(ray, results);
+
+
+
+//        CollisionResults results = new CollisionResults();
+        // 2. Aim the ray from cam loc to cam direction.
+  //      Ray ray = new Ray(cam.getLocation(), cam.getDirection());
+        // 3. Collect intersections between Ray and Shootables in results list.
+        sceneModel.collideWith(ray, results);
+        // 4. Print the results
+        System.out.println("----- Collisions? " + results.size() + "-----");
+        Vector3f pt=null;
+        for (int i = 0; i < results.size(); i++) {
+          // For each hit, we know distance, impact point, name of geometry.
+          float dist = results.getCollision(i).getDistance();
+          pt = results.getCollision(i).getContactPoint();
+          String hit = results.getCollision(i).getGeometry().getName();
+          System.out.println("* Collision #" + i);
+          System.out.println("  You shot " + hit + " at " + pt + ", " + dist + " wu away.");
+        }
+        System.out.println("sinbad "+sinbadPlayer.getLocalTranslation());
+        // 5. Use the results (we mark the hit object)
+        
+        
+       // if(arrow!=null) rootNode.detachChild(arrow);
+        removeArrow();
+        putShape(arrow, ColorRGBA.Green).setLocalTranslation(new Vector3f(pt.x, pt.y,pt.z));
+        
+        //rootNode.detachChild(sceneModel);
+
+///
+
+    }
     @Override
     public void simpleInitApp() {
        
@@ -110,9 +195,11 @@ public class Main extends SimpleApplication implements AnalogListener,AnimEventL
        flyCam.setEnabled(false);
        ChaseCamera chaseCam = new ChaseCamera(cam, sinbadPlayer, inputManager);
       
-        bulletAppState.getPhysicsSpace().enableDebug(assetManager);
-        afficherTexte("Version d'essaie");
-      //  initSimpleWater();initPPcWater();
+       // bulletAppState.getPhysicsSpace().enableDebug(assetManager);
+        afficherTexte("Version d'essai");
+       initSimpleWater();initPPcWater();       
+      
+ 
     }
     
     
@@ -142,6 +229,10 @@ public class Main extends SimpleApplication implements AnalogListener,AnimEventL
        
         
         bulletAppState.getPhysicsSpace().add(player);
+        
+        
+         
+       
     }    
     
     
@@ -158,34 +249,32 @@ public class Main extends SimpleApplication implements AnalogListener,AnimEventL
     }
     
      public void onAnimCycleDone(AnimControl control, AnimChannel channel, String animName) {
-    if (animName.equals("walk")) {
+    if (animName.equals("walk")&&!up) {
       channel.setAnim("idle");
       
     }
+    else channel.setAnim("walk");
   }
    
      
-     private ActionListener actionListener = new ActionListener() { 
-     public void onAction(String name, boolean keyPressed, float tpf) {
-     if (name.equals("addObject")) {
-          //Node fireCamp=(Node) assetManager.loadModel("Models/Oto/Oto.mesh.xml");
-          Spatial fireCamp = assetManager.loadModel("Models/campfire/campfire.j3o");
-          rootNode.attachChild(fireCamp);
-          fireCamp.setLocalTranslation(new Vector3f(player.getPhysicsLocation().x+30,player.getPhysicsLocation().y,player.getPhysicsLocation().z));
-          System.out.println("child attached !!");
-          
-      }
-       System.out.println("This is onAction");
-     }
-   };
-      
-     public void onAnalog(String binding, float isPressed, float tpf) {
+     private ActionListener actionListener;
+     
+    
+     
+     
+     private int verifyUpAnalog1=0; 
+     private int verifyUpAnalog2=0; 
+  public void onAnalog(String binding, float isPressed, float tpf) {
+         turnCharacterDependentlyOnCam();
     if (binding.equals("Left")) {
       left = true;
+      
     } else if (binding.equals("Right")) {
       right= true;
+      
     } else if (binding.equals("Up")) {
       up = true;
+      verifyUpAnalog1++;
    // sinbadPlayer.rotateUpTo(new Vector3f(player.getViewDirection().x,cam.getRotation().getY(),sinbadPlayer.getLocalRotation().getZ()));
       
     } else if (binding.equals("Down")) {
@@ -193,6 +282,7 @@ public class Main extends SimpleApplication implements AnalogListener,AnimEventL
     } else if (binding.equals("Jump")) {
        player.jump(); 
     }
+    
     
   // System.out.println("walk "+ isPressed +" "+ binding);
         if (up && !channel.getAnimationName().equals("walk")) {
@@ -203,10 +293,17 @@ public class Main extends SimpleApplication implements AnalogListener,AnimEventL
     }
   
      
+   private float turned=0;
+   public  void turnCharacterDependentlyOnCam()
+   { if(turned==0) turned=walkDirection.y;
+       System.out.println("walkDirection = "+cam.getRotation().getY()+"  "+sinbadPlayer.getLocalRotation().getY());
+       Quaternion q=sinbadPlayer.getLocalRotation();
+       sinbadPlayer.setLocalRotation(new Quaternion(q.getX(),cam.getRotation().getY(),q.getZ(),cam.getRotation().getW()));
+    //  sinbadPlayer.rotate(0, ((int)walkDirection.y)*30, 0); 
+  }
      
-     
-     private Vector3f lastPostion=new Vector3f(0,0,0);
-
+    
+   
     @Override
     public void simpleUpdate(float tpf) {
         //TODO: add update code
@@ -220,6 +317,7 @@ public class Main extends SimpleApplication implements AnalogListener,AnimEventL
         if (left) {
             walkDirection.addLocal(camLeft);
             left=false;
+        
                   }
         if (right) {
             walkDirection.addLocal(camLeft.negate());
@@ -227,47 +325,58 @@ public class Main extends SimpleApplication implements AnalogListener,AnimEventL
         }
         if (up) {
             walkDirection.addLocal(camDir);
-            up=false;
+           
+            if(verifyUpAnalog1 ==verifyUpAnalog2){up=false; verifyUpAnalog1 =verifyUpAnalog2=0; }
+            else
+                verifyUpAnalog2=verifyUpAnalog1;
         }
         if (down) {
             walkDirection.addLocal(camDir.negate());
             down=false;
         }
         
-        player.setWalkDirection(walkDirection);
-        sinbadPlayer.rotate(0, 2*tpf, 0);
+       player.setWalkDirection(walkDirection);
+        
      
         /*cam.setLocation(new Vector3f(player.getPhysicsLocation().x,
                 player.getPhysicsLocation().y+15,
                 player.getPhysicsLocation().z
                 )); */
         
-        /// modifaction de la la position du player 
+      /// modifaction de la la position du player 
+        
         sinbadPlayer.setLocalTranslation(new Vector3f(
-                player.getPhysicsLocation().x, player.getPhysicsLocation().y-2.5f,
-                player.getPhysicsLocation().z));
+        player.getPhysicsLocation().x, player.getPhysicsLocation().y-2.5f,
+        player.getPhysicsLocation().z));
         
       // System.out.println(walkDirection.toString());
       
         
-         lastPostion=player.getPhysicsLocation();
+       
          
     }
     
     /** We over-write some navigational key mappings here, so we can
    * add physics-controlled walking and jumping: */
   private void setUpKeys() {
-    inputManager.addMapping("Left", new  KeyTrigger(KeyInput.KEY_A));
-    inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_D));
-    inputManager.addMapping("Up", new KeyTrigger(KeyInput.KEY_W));
-    inputManager.addMapping("Down", new KeyTrigger(KeyInput.KEY_S));
+    inputManager.addMapping("Left", new  KeyTrigger(KeyInput.KEY_LEFT));
+    inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_RIGHT));
+    inputManager.addMapping("Up", new KeyTrigger(KeyInput.KEY_UP));
+    inputManager.addMapping("Down", new KeyTrigger(KeyInput.KEY_DOWN));
     inputManager.addMapping("Jump", new KeyTrigger(KeyInput.KEY_SPACE));
     inputManager.addMapping("addObject", new KeyTrigger(KeyInput.KEY_X));
+    
+   inputManager.addMapping("LClick", new MouseButtonTrigger(0));         // Left-button click
+   inputManager.addMapping("RClick", new MouseButtonTrigger(1));         // Right-button click
+
+    
+    
     inputManager.addListener(this, "Left");
     inputManager.addListener(this, "Right");
     inputManager.addListener(this, "Up");
     inputManager.addListener(this, "Down");
     inputManager.addListener(this, "Jump");
+    inputManager.addListener(actionListener,"LClick");
     inputManager.addListener(actionListener, "addObject");
   }
     
@@ -280,7 +389,7 @@ public class Main extends SimpleApplication implements AnalogListener,AnimEventL
            /** A white ambient light source. */ 
     AmbientLight ambient = new AmbientLight();
     ambient.setColor(ColorRGBA.White);
-    rootNode.addLight(ambient); 
+   rootNode.addLight(ambient); 
     
     DirectionalLight dl = new DirectionalLight();
     dl.setColor(ColorRGBA.White);
@@ -291,16 +400,17 @@ public class Main extends SimpleApplication implements AnalogListener,AnimEventL
      
    private void initScene()
    {
-       sceneModel=assetManager.loadModel("Scenes/scene1.j3o");
+     sceneModel=assetManager.loadModel("Scenes/scene1.j3o");
        
        
      //  assetManager.registerLocator("newScene.zip", ZipLocator.class);
-    //sceneModel = assetManager.loadModel("Models/terrain1/terrain1.j3o");
+    //sceneModel = assetManager.loadModel("Models/terrain2/terrain2.j3o"); sceneModel.setLocalScale(10f, 2f, 10f);
+    
     rootNode.attachChild(sceneModel);
      
        
-    sceneModel.setLocalTranslation(0, 0, 0);
-    //sceneModel.setLocalScale(2f, 1f, 2f);
+    sceneModel.setLocalTranslation(0, 2, 0);
+    
       //initTerrain();
     //initSimpleWater();
      //initPPcWater();
@@ -311,9 +421,25 @@ public class Main extends SimpleApplication implements AnalogListener,AnimEventL
         sceneModel.addControl(landscape);  //define the scene as a rigid body
         
         bulletAppState.getPhysicsSpace().add(landscape);
+              
+       
     
    }
+   Geometry g=null;
+private Geometry putShape(Mesh shape, ColorRGBA color){
+  g = new Geometry("coordinate axis", shape);
+  Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+  mat.getAdditionalRenderState().setWireframe(true);
+  mat.setColor("Color", color);
+  g.setMaterial(mat);
+  rootNode.attachChild(g);
+  return g;
+}
 
+private void removeArrow()
+{    if(g!=null)
+    rootNode.detachChild(g);
+}
     @Override
     public void simpleRender(RenderManager rm) {
         
@@ -342,7 +468,7 @@ public class Main extends SimpleApplication implements AnalogListener,AnimEventL
     Vector3f waterLocation=new Vector3f(0,-6,0); 
     waterProcessor.setPlane(new Plane(Vector3f.UNIT_Y, waterLocation.dot(Vector3f.UNIT_Y)));
     viewPort.addProcessor(waterProcessor); waterProcessor.setWaterDepth(10); // transparency of water 
-    waterProcessor.setDistortionScale(0.05f); // strength of waves 
+    waterProcessor.setDistortionScale(0.07f); // strength of waves 
     waterProcessor.setWaveSpeed(0.05f); // speed of waves Quad 
     quad = new Quad(800,800); 
     quad.scaleTextureCoordinates(new Vector2f(6f,6f)); 
@@ -372,7 +498,7 @@ public class Main extends SimpleApplication implements AnalogListener,AnimEventL
    private TerrainQuad terrain;
   Material mat_terrain;
    
-   private void initTerrain()
+   private void initTerrain() // essaye de charger un terrain à partir d'un height map
    {
        /** 1. Create terrain material and load four textures into it. */
     mat_terrain = new Material(assetManager, 
