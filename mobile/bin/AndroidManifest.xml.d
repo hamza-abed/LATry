9657828979637470bd016539eddc2054ce4edbc6
hamzaf16 <hamza.abed.professionel@gmail.com>
2014-04-04 10:41:36 +0200

    Début de reproduction de la boussole du jeux, Changement du nom du projet LA-Client

--- mobile/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\applications\workspaces\withJMEOriginalSDK\chargementDynamique\mobile\bin\AndroidManifest.xml : \
-C:\applications\workspaces\withJMEOriginalSDK\chargementDynamique\mobile\AndroidManifest.xml \
