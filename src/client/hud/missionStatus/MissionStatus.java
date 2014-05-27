0401f6c793126c6fe780fb0fc3e402ac66347ab2
hamzaf16 <hamza.abed.professionel@gmail.com>
2014-05-27 11:31:03 +0200

    *Ajout de la methode setAvancementMission(%) pour encapsuler l'objet missionStatus et faciliter son utilisation.
    *Ajout du menu en bas de l'interface graphique du jeu dans nifty pour terminer les fonctionnalitées du jeu.

--- src/client/hud/missionStatus/MissionStatus.java
+++ src/client/hud/missionStatus/MissionStatus.java
@@ -1,7 +1,42 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Copyright 2010 http://learning-adventure.fr
+ * Tous droits réservés
+ * 
+ * 
+ * ----------------------------------------------------------------------------
+ * Ce fichier fait partie de LA-Client.
+ *
+ * LA-Client est un logiciel libre ; vous pouvez le redistribuer ou le modifier 
+ * suivant les termes de la GNU General Public License telle que publiée par
+ * la Free Software Foundation ; soit la version 3 de la licence, soit 
+ * (à votre gré) toute version ultérieure.
+ * 
+ * LA-Client est distribué dans l'espoir qu'il sera utile, 
+ * mais SANS AUCUNE GARANTIE ; pas même la garantie implicite de 
+ * COMMERCIABILISABILITÉ ni d'ADÉQUATION à UN OBJECTIF PARTICULIER. 
+ * Consultez la GNU General Public License pour plus de détails.
+ * 
+ * Vous devez avoir reçu une copie de la GNU General Public License 
+ * en même temps que LA-Client ; si ce n'est pas le cas, 
+ * consultez <http://www.gnu.org/licenses>.
+ * ----------------------------------------------------------------------------
+ * This file is part of LA-Client.
+ *
+ * LA-Client is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LA-Client is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with LA-Client.  If not, see <http://www.gnu.org/licenses/>.
+ * ----------------------------------------------------------------------------
  */
+
 package client.hud.missionStatus;
 
 import com.jme3.font.BitmapText;
@@ -11,16 +46,20 @@ import com.jme3.math.ColorRGBA;
 import com.jme3.math.FastMath;
 import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
-import com.jme3.post.FilterPostProcessor;
-import com.jme3.post.filters.BloomFilter;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 import com.jme3.scene.shape.Box;
-import com.jme3.texture.Texture;
 import shared.variables.Variables;
 /**
- *
- * @author admin
+ * Cette classe représente un noeud graphique qui est utile pour être attaché 
+ * à l'interface 2D du jeu et représente un Loading Bar composé de
+ *<ul>
+ * <li> 2 cubes, l'un à l'intérieur de l'autre </li>
+ * <li> Un noeud textuel qui permet d'fficher le message de status de la mission</li>
+ * <li> la méthode setAvancementMission(x) prend la valeur du pourcentage d'avancement dans la mission</li>
+ * </ul>
+ * 
+ * @author Hamza ABED 2014 hamza.abed.professionel@gmail.com
  */
 public class MissionStatus extends Node{
     
@@ -35,7 +74,7 @@ public class MissionStatus extends Node{
     private Geometry statusBoxGeo;
     private Geometry centerOfStatusBoxGeo;
     private float initialScale;
-    
+    private BitmapText text;
     public MissionStatus()
     {
         super("missionStatus");
@@ -51,7 +90,7 @@ public class MissionStatus extends Node{
                 "Common/MatDefs/Misc/Unshaded.j3md");
               
         mat1.setTexture("ColorMap", Variables.getLaGame().getAssetManager().
-                loadTexture("Textures/cage.png"));
+                loadTexture("Textures/verre.png"));
         mat1.getAdditionalRenderState().setBlendMode(RenderState.BlendMode.Alpha);
    
     statusBoxGeo.setMaterial(mat1);
@@ -61,7 +100,7 @@ public class MissionStatus extends Node{
     /*******************Center Box **************************************/
     /********************************************************************/
     
-     Box center= new Box(centerOfTheBox,centerBoxHeight, centerBoxHeight, centerBoxHeight);
+     Box center= new Box(centerOfTheBox,boxHeight, boxHeight, boxHeight);
        centerOfStatusBoxGeo = new Geometry("statusBox", center);
       Material mat2 = new Material(Variables.getLaGame().getAssetManager(),
                 "Common/MatDefs/Misc/Unshaded.j3md");
@@ -83,7 +122,7 @@ public class MissionStatus extends Node{
     /*************************************************************************/
       
        String txt = "Avancement 10%";
-       BitmapText text = new BitmapText(Variables.getLaGame().getGuiFont());
+       text = new BitmapText(Variables.getLaGame().getGuiFont());
        text.setSize(Variables.getLaGame().getGuiFont().getCharSet().getRenderedSize());
        text.setText(txt);
        text.setLocalTranslation(statusBoxGeo.getLocalTranslation().x - boxHeight*1.3f,
@@ -92,16 +131,15 @@ public class MissionStatus extends Node{
        
        
        this.attachChild(text);
-  
+  setAvancementMission(80);
    }
     
     private float variation=0.000f;
-    private int retardeurDeuxFrames =0;
+  
     public void update(float tpf) {
 
         
-        if(retardeurDeuxFrames==0)retardeurDeuxFrames=1;
-        else retardeurDeuxFrames=0;
+       
         if (ROTATION > 10) {
             ROTATION = 0.1f;
 
@@ -111,7 +149,7 @@ public class MissionStatus extends Node{
         //	Matrix3f m=new Matrix3f(0, 0, ROTATION, 0, 0, 0, 0, 0, 0);
         statusBoxGeo.setLocalRotation(new Quaternion(ROTATION, 0.1f, 0.2f, 0.8f));
         centerOfStatusBoxGeo.setLocalRotation(new Quaternion(ROTATION, 0.1f, 0.2f, 0.8f));
-        variation += 0.0001f*retardeurDeuxFrames;
+        variation += 0.0001f;
         
         if (variation > 0.005f) {
             variation = 0.001f;
@@ -120,8 +158,22 @@ public class MissionStatus extends Node{
 
         }
         
-        centerOfStatusBoxGeo.setLocalScale(centerOfStatusBoxGeo.getLocalScale().x+variation);
+       // centerOfStatusBoxGeo.setLocalScale(centerOfStatusBoxGeo.getLocalScale().x+variation);
         centerOfStatusBoxGeo.setLocalRotation(new Quaternion(ROTATION, 0.1f, 0.2f, 0.8f));
+    }
+    
+    
+    
+        public void setAvancementMission(float x) {
+            if(x>100)x=100;
+            if(x<0)x=0;
+        //la valeur de x reçu est en %
+        float realValue = x * statusBoxGeo.getLocalScale().x / 100;
+            
+
+        centerOfStatusBoxGeo.setLocalScale(realValue);
+        initialScale = centerOfStatusBoxGeo.getLocalScale().x; // valeur utilisée dans la vibration du cube au centre
+         text.setText("Avancement "+x+"%");
     }
 }
     
